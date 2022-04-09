172fa3c94f373d7cbe67e0d428940ffec1836cac
delkabo <delkabo@yandex.ru>
2022-04-09 13:54:36 +0300

    added five rest-api tests

---
+++ src/test/java/tests/SimpleTests.java
@@ -0,0 +1,81 @@
+package tests;
+
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.is;
+
+public class SimpleTests {
+
+//    @Test
+//    void listUserTest() {
+//        given()
+//                .contentType(JSON)
+//                .when()
+//                .get("https://reqres.in/api/users?page=2")
+//                .then()
+//                .body("data.last_name",is("Lawson"));
+//    }
+
+    @Test
+    void singleUserTest(){
+        given()
+                .when()
+                .get("https://reqres.in/api/users/2")
+                .then()
+                .body("data.last_name", is("Weaver"));
+    }
+
+    @Test
+    void singleUserNotFoundTest(){
+        given()
+                .when()
+                .get("https://reqres.in/api/users/23")
+                .then()
+                .statusCode(404);
+    }
+
+    String userData = "{ \"name\": \"morpheus\", \"job\": \"leader\" }";
+
+    @Test
+    void createUserTest() {
+        given()
+                .body(userData)
+                .contentType(JSON)
+                .when()
+                .post("https://reqres.in/api/users")
+                .then()
+                .statusCode(201);
+    }
+
+    String userData1 = "{ \"name\": \"morpheus\", \"job\": \"zion resident\" }";
+
+    @Test
+    void updateUserTest() {
+        given()
+                .body(userData1)
+                .contentType(JSON)
+                .when()
+                .post("https://reqres.in/api/users/2")
+                .then()
+                .body("name", is("morpheus"));
+    }
+
+    String userData2 = "{ \"email\": \"sydney@fife\" }";
+
+    @Test
+    void registerUserTest() {
+        given()
+                .body(userData2)
+                .contentType(JSON)
+                .when()
+                .post("https://reqres.in/api/register")
+                .then()
+                .statusCode(400)
+                .body("error", is("Missing password"));
+    }
+
+
+}
